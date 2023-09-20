31acffc8e4dcfe09c1a09d677865508a37716725
Thomas Thomassen <thomas@thomthom.net>
2023-09-20 18:48:49 +0200

    Rename `buffer_sum` to `chuck_sum` to better match comment description.

--- lib/roman/parser.rb
+++ lib/roman/parser.rb
@@ -11,25 +11,25 @@ class RomanNumeral < Numeric
     def process(tokens)
       sum = 0
       previous_value = 0
-      buffer_sum = 0
+      chuck_sum = 0
       tokens.each { |token|
         # Read the characters in chunks. Each chunk consists of the same token.
         # Add up the sum for the chunk and compare against the previous token
         # chunk whether to add or subtract to the total sum.
         value = token.value
         if value == previous_value
-          buffer_sum += value
+          chuck_sum += value
         else
           if value > previous_value && previous_value > 0
-            sum -= buffer_sum
+            sum -= chuck_sum
           else
-            sum += buffer_sum
+            sum += chuck_sum
           end
-          buffer_sum = value
+          chuck_sum = value
         end
         previous_value = value
       }
-      sum + buffer_sum
+      sum + chuck_sum
     end
 
   end
